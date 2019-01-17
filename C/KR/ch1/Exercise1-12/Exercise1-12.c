ad63e448e4870a052cc70e7154b9ad63f59a8096
tipsy1227 <tipsy1227@gmail.com>
2019-01-17 18:19:28 +0800

    Add Exercise1-12

---
+++ C/KR/ch1/Exercise1-12/Exercise1-12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+/*copy the input one word per line to the output*/
+int main(){
+	int c, state;
+
+	state = OUT;
+	while((c=getchar()) != EOF)
+		if(c!=' ' && c!='\n' && c!='\t'){
+			state = IN;
+			putchar(c);
+		} else if(state == IN){
+			state = OUT;
+			putchar('\n');
+		}
+}
---
+++ C/KR/ch1/Exercise1-12/README.md
@@ -0,0 +1,8 @@
+**Exercise 1-12** Write a program that prints its input one word per line.
+
+**筆記:**\
+這個練習與書中的word-count程式非常相似，我們可以用類似的想法來實作這個程式。
+
+**Brief explination:**\
+This exercise is quite similar to the word-count program in the book. We can
+use the identical concept to implement this exercise.
