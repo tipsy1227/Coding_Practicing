1b3fbebaa5406b0e1fb05179126b46ea255b668c
tipsy1227 <tipsy1227@gmail.com>
2019-02-11 19:07:37 +0800

    Add Exercise3-2

---
+++ C/KR/ch3/Exercise3-2/Exercise3-2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int mygetline(char line[], int maxline);    /* mygetline: read a line into s, return length */
+void escape(char s[], char t[]);            /* escape: converts newline and tab into visible escape \n and \t */
+
+/* copies input to output and converts newline and tab into \n and \t */
+int main(){
+	int c, len;
+	char s[MAXLINE], t[MAXLINE];
+
+	while((len=mygetline(s, MAXLINE))>0){
+		escape(s, t);
+		printf("%s\n", t);
+	}
+	return 0;
+}
+
+int mygetline(char s[], int lim){
+	int c, i;
+
+	for(i=0; i<lim-2 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+
+	if(c!=EOF){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+
+	// keep taking characters until it is EOF or '\n'
+	while(c!=EOF && c!='\n'){
+		c=getchar();
+		i++;
+	}
+	return i;
+}
+
+void escape(char s[], char t[]){
+	int c, i, j;
+
+	for(i=j=0; (c=s[i])!='\0'; i++)
+		switch(c){
+			case '\n':
+				t[j++] = '\\';
+				t[j++] = 'n';
+				break;
+			case '\t':
+				t[j++] = '\\';
+				t[j++] = 't';
+				break;
+			default:
+				t[j++] = c;
+				break;
+		}
+	t[j] = '\0';
+}
