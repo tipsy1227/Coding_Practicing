955aba38036b59e7c27eb3e1bdd1f0dcb138f214
tipsy1227 <tipsy1227@gmail.com>
2019-02-25 21:17:08 +0800

    Add Exercise5-5

---
+++ C/KR/ch5/Exercise5-5/Exercise5-5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXBUF 100
+
+char *mystrncpy(char *, char *, int);
+char *mystrncat(char *, char *, int);
+int mystrncmp(char *, char *, int);
+
+/* Implement the library functions strncpy, strncat, and strncmp */
+int main(){
+	char s1[MAXBUF] = "Hello, World";
+	printf("%s\n", mystrncpy(s1, "HELLO", 5));
+
+	char s2[MAXBUF] = "Hello, ";
+	printf("%s\n", mystrncat(s2, "World!\n", 5));
+
+	char s3[MAXBUF] = "Hello, World";
+	char s4[MAXBUF] = "Hello, World!\n";
+	printf("%d\n", mystrncmp(s3, s4, 12));
+	return 0;
+}
+
+/* mystrncpy: copies at most n characters of t to s */
+char *mystrncpy(char *s, char *t, int n){
+	int i;
+	char *u = s;
+
+	for(i=0; i<n&&*t!='\0'; i++, u++, t++)
+		*u=*t;
+	return s;
+}
+
+/* mystrncat: copies the string t to the end of s */
+char *mystrncat(char *s, char *t, int n){
+	char *u = s;
+	int i;
+
+	while(*u!='\0')
+		u++;
+	for(i=0; i<n&&(*u=*t)!='\0'; i++, u++, t++)
+		;
+	return s;
+}
+
+/* mystrncmp: compares the string s and s, returns 0 if they are the same  */
+int mystrncmp(char *s, char *t, int n){
+	int i;
+	for(i=0; i<n&&*s!='\0'&&*t!='\0'&&*s==*t; i++, s++, t++)
+		;
+	return *s-*t;
+}
---
+++ C/KR/ch5/Exercise5-5/README.md
@@ -0,0 +1,3 @@
+**Exercise 5-5.** Write versions of the library functions strncpy, strncat, and strncmp, which operate on at most 
+the first n characters of their argument strins. For example, strncpy(s, t, n) copies at most n characters of t to 
+s. Full descriptions are in Appendix B.
